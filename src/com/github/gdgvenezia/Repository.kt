5d5abfdddfd396e123a3664fa3f0902c357124a6
Andrea Maglie <andrea.maglie@gmail.com>
2019-10-24 18:15:55 +0200

    changed response model for lists

--- src/com/github/gdgvenezia/Repository.kt
+++ src/com/github/gdgvenezia/Repository.kt
@@ -2,8 +2,12 @@ package com.github.gdgvenezia
 
 interface Repository {
     suspend fun getEvents(): List<EventModel>
-    fun getPhotos(): List<PhotoModel>
+    fun getPhotos(): ListResponseModel<PhotoModel>
     fun getTeam(): List<TeamMemberModel>
     fun getSocialLinks(): List<SocialLinkModel>
 
 }
+
+data class ListResponseModel<T>(
+    val items: List<T>
+)
